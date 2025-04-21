37f44e751fd811634ae9c825e2022269e914c496
George Davis <gtdavis25@gmail.com>
2025-04-21 11:29:47 +0100

    2024 day 3

---
+++ 2024/day03/day03.go
@@ -0,0 +1,53 @@
+package day03
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gtdavis25/aoc/solver"
+)
+
+type Solver struct{}
+
+func NewSolver(_ solver.Params) *Solver {
+	return &Solver{}
+}
+
+func (s *Solver) Solve(lines []string) (solver.Result, error) {
+	var part1 int
+	for _, line := range lines {
+		for i := range len(line) {
+			var x, y int
+			if _, err := fmt.Sscanf(line[i:], "mul(%d,%d)", &x, &y); err == nil {
+				part1 += x * y
+			}
+		}
+	}
+
+	var part2 int
+	var disabled bool
+	for _, line := range lines {
+		for i := range len(line) {
+			switch {
+			case strings.HasPrefix(line[i:], "don't()"):
+				disabled = true
+
+			case strings.HasPrefix(line[i:], "do()"):
+				disabled = false
+
+			case !disabled:
+				var x, y int
+				if _, err := fmt.Sscanf(line[i:], "mul(%d,%d)", &x, &y); err == nil {
+					part2 += x * y
+				}
+
+			default:
+			}
+		}
+	}
+
+	return solver.Result{
+		Part1: part1,
+		Part2: part2,
+	}, nil
+}
--- solvers.go
+++ solvers.go
@@ -6,6 +6,7 @@ import (
 
 	year2024day01 "github.com/gtdavis25/aoc/2024/day01"
 	year2024day02 "github.com/gtdavis25/aoc/2024/day02"
+	year2024day03 "github.com/gtdavis25/aoc/2024/day03"
 )
 
 type SolverFactory func(solver.Params) solver.Interface
@@ -17,6 +18,9 @@ var Solvers = map[int]map[int]SolverFactory{
 		},
 		2: func(params solver.Params) solver.Interface {
 			return year2024day02.NewSolver(params)
+		},
+		3: func(params solver.Params) solver.Interface {
+			return year2024day03.NewSolver(params)
 		},
 	},
 }
