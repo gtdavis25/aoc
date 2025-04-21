50dad9ceb6ad6f361a5b9cdff08d8f6f2f7b319b
George Davis <gtdavis25@gmail.com>
2025-04-21 14:44:21 +0100

    2024 day 7 part 2

--- 2024/day07/day07.go
+++ 2024/day07/day07.go
@@ -41,35 +41,79 @@ func (s *Solver) Solve(lines []string) (solver.Result, error) {
 
 	var part1 int
 	for result, operands := range equations {
-		if canMake(result, operands) {
+		if canMake(result, operands, []operator{add, multiply}) {
 			part1 += result
+		}
+	}
+
+	var part2 int
+	for result, operands := range equations {
+		if canMake(result, operands, []operator{add, multiply, concatenate}) {
+			part2 += result
 		}
 	}
 
 	return solver.Result{
 		Part1: part1,
+		Part2: part2,
 	}, nil
 }
 
-func canMake(result int, operands []int) bool {
-	switch {
-	case len(operands) == 0:
-		return false
+type operator func(int, int) int
 
-	case len(operands) == 1:
-		return operands[0] == result
+func add(x, y int) int {
+	return x + y
+}
 
-	default:
-		last := operands[len(operands)-1]
-		rest := operands[:len(operands)-1]
-		if d := result - last; d >= 0 && canMake(d, rest) {
-			return true
-		}
+func multiply(x, y int) int {
+	return x * y
+}
 
-		if result%last == 0 && canMake(result/last, rest) {
-			return true
-		}
+func concatenate(x, y int) int {
+	b := 10
+	for y%b != y {
+		b *= 10
+	}
 
+	return x*b + y
+}
+
+func canMake(result int, operands []int, operators []operator) bool {
+	if len(operands) == 0 {
 		return false
 	}
+
+	stack := []state{
+		{
+			acc:   operands[0],
+			index: 0,
+		},
+	}
+
+	for len(stack) > 0 {
+		s := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if s.acc == result && s.index+1 == len(operands) {
+			return true
+		}
+
+		if s.index+1 == len(operands) {
+			continue
+		}
+
+		for _, operator := range operators {
+			next := operator(s.acc, operands[s.index+1])
+			stack = append(stack, state{
+				acc:   next,
+				index: s.index + 1,
+			})
+		}
+	}
+
+	return false
+}
+
+type state struct {
+	acc   int
+	index int
 }
