1dda5435a8b8287fe8c796fd1d8f890d5134edee
George Davis <gtdavis25@gmail.com>
2025-04-22 22:14:02 +0100

    2024 day 10 part 2

--- 2024/day10/day10.go
+++ 2024/day10/day10.go
@@ -14,76 +14,85 @@ func NewSolver(_ solver.Params) *Solver {
 }
 
 func (s *Solver) Solve(lines []string) (solver.Result, error) {
-	var queue []trail
-	for y := range lines {
-		for x := range len(lines[y]) {
-			if lines[y][x] == '0' {
-				queue = append(queue, trail{
-					start: geom2d.Point{
-						X: x,
-						Y: y,
-					},
-					end: geom2d.Point{
-						X: x,
-						Y: y,
-					},
-				})
-			}
-		}
-	}
-
-	trails := make(map[geom2d.Point][]geom2d.Point)
-	for i := 0; i < len(queue); i++ {
-		t := queue[i]
-		if lines[t.end.Y][t.end.X] == '9' {
-			if !slices.Contains(trails[t.start], t.end) {
-				trails[t.start] = append(trails[t.start], t.end)
-			}
-
-			continue
-		}
-
-		for _, p := range []geom2d.Point{
-			{
-				X: t.end.X,
-				Y: t.end.Y - 1,
-			},
-			{
-				X: t.end.X + 1,
-				Y: t.end.Y,
-			},
-			{
-				X: t.end.X,
-				Y: t.end.Y + 1,
-			},
-			{
-				X: t.end.X - 1,
-				Y: t.end.Y,
-			},
-		} {
-			if p.Y < 0 || len(lines) <= p.Y || p.X < 0 || len(lines[p.Y]) <= p.X ||
-				lines[p.Y][p.X]-lines[t.end.Y][t.end.X] != 1 {
-				continue
-			}
-
-			queue = append(queue, trail{
-				start: t.start,
-				end:   p,
-			})
-		}
-	}
-
-	var part1 int
-	for _, ends := range trails {
-		part1 += len(ends)
+	trailheads := getTrailheads(lines)
+	var part1, part2 int
+	for _, trailhead := range trailheads {
+		part1 += trailhead.score
+		part2 += trailhead.rating
 	}
 
 	return solver.Result{
 		Part1: part1,
+		Part2: part2,
 	}, nil
 }
 
-type trail struct {
-	start geom2d.Point
-	end   geom2d.Point
+type trailhead struct {
+	score  int
+	rating int
+}
+
+func getTrailheads(lines []string) []trailhead {
+	var trailheads []trailhead
+	for y, line := range lines {
+		for x := range len(line) {
+			if lines[y][x] != '0' {
+				continue
+			}
+
+			trailEnds := getTrailEnds(lines, geom2d.Point{X: x, Y: y})
+			trailheads = append(trailheads, trailhead{
+				score:  distinctCount(trailEnds),
+				rating: len(trailEnds),
+			})
+		}
+	}
+
+	return trailheads
+}
+
+func getTrailEnds(lines []string, p geom2d.Point) []geom2d.Point {
+	if lines[p.Y][p.X] == '9' {
+		return []geom2d.Point{p}
+	}
+
+	var trailEnds []geom2d.Point
+	for _, next := range []geom2d.Point{
+		{
+			X: p.X,
+			Y: p.Y - 1,
+		},
+		{
+			X: p.X + 1,
+			Y: p.Y,
+		},
+		{
+			X: p.X,
+			Y: p.Y + 1,
+		},
+		{
+			X: p.X - 1,
+			Y: p.Y,
+		},
+	} {
+		if next.Y < 0 || next.Y >= len(lines) || next.X < 0 || next.X >= len(lines[next.Y]) ||
+			lines[next.Y][next.X]-lines[p.Y][p.X] != 1 {
+			continue
+		}
+
+		trailEnds = append(trailEnds, getTrailEnds(lines, next)...)
+	}
+
+	return trailEnds
+}
+
+func distinctCount(points []geom2d.Point) int {
+	var count int
+	for i := range points {
+		if !slices.Contains(points[:i], points[i]) {
+			count++
+		}
+	}
+
+	return count
 }
