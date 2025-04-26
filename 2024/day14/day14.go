b6a61d2e2933fd724a299db9259fca55402a7614
George Davis <gtdavis25@gmail.com>
2025-04-26 21:16:47 +0100

    2024 day 14 part 2

--- 2024/day14/day14.go
+++ 2024/day14/day14.go
@@ -2,6 +2,7 @@ package day14
 
 import (
 	"fmt"
+	"slices"
 
 	"github.com/gtdavis25/aoc/geom2d"
 	"github.com/gtdavis25/aoc/solver"
@@ -14,37 +15,23 @@ func NewSolver(_ solver.Params) *Solver {
 }
 
 func (s *Solver) Solve(lines []string) (solver.Result, error) {
-	robots := make([]robot, len(lines))
+	initial := make([]robot, len(lines))
 	for i, line := range lines {
 		var pos, vel geom2d.Point
 		if _, err := fmt.Sscanf(line, "p=%d,%d v=%d,%d", &pos.X, &pos.Y, &vel.X, &vel.Y); err != nil {
 			return solver.Result{}, fmt.Errorf("parsing %q on line %d: %w", line, i, err)
 		}
 
-		robots[i] = robot{
+		initial[i] = robot{
 			pos: pos,
 			vel: vel,
 		}
 	}
 
 	width, height := 101, 103
+	robots := slices.Clone(initial)
 	for range 100 {
-		for i, r := range robots {
-			x := (r.pos.X + r.vel.X) % width
-			if x < 0 {
-				x += width
-			}
-
-			y := (r.pos.Y + r.vel.Y) % height
-			if y < 0 {
-				y += height
-			}
-
-			robots[i].pos = geom2d.Point{
-				X: x,
-				Y: y,
-			}
-		}
+		nextState(robots, width, height)
 	}
 
 	var quadrants [2][2]int
@@ -64,12 +51,51 @@ func (s *Solver) Solve(lines []string) (solver.Result, error) {
 		}
 	}
 
+	var part2 int
+	robots = slices.Clone(initial)
+	for hasDuplicatePositions(robots) {
+		nextState(robots, width, height)
+		part2++
+	}
+
 	return solver.Result{
 		Part1: quadrants[0][0] * quadrants[0][1] * quadrants[1][0] * quadrants[1][1],
+		Part2: part2,
 	}, nil
 }
 
 type robot struct {
 	pos geom2d.Point
 	vel geom2d.Point
+}
+
+func nextState(robots []robot, width, height int) {
+	for i, r := range robots {
+		x := (r.pos.X + r.vel.X) % width
+		if x < 0 {
+			x += width
+		}
+
+		y := (r.pos.Y + r.vel.Y) % height
+		if y < 0 {
+			y += height
+		}
+
+		robots[i].pos = geom2d.Point{
+			X: x,
+			Y: y,
+		}
+	}
+}
+
+func hasDuplicatePositions(robots []robot) bool {
+	for i := range robots {
+		for j := range robots[:i] {
+			if robots[i].pos == robots[j].pos {
+				return true
+			}
+		}
+	}
+
+	return false
 }
