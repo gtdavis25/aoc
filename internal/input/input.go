104226b94713c64f0d9a338960ecd2d75891a406
George Davis <gtdavis25@gmail.com>
2025-04-29 21:51:07 +0100

    add input package

---
+++ internal/input/input.go
@@ -0,0 +1,20 @@
+package input
+
+import (
+	"bufio"
+	"io"
+)
+
+func ReadLines(r io.Reader) ([]string, error) {
+	var lines []string
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
